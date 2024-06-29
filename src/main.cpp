26842bc0a0f73e89277497bdca9b2cbbe0e238b7
rakshitraj <hello@rakshitraj.com>
2024-06-30 02:35:03 +0530

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 
+void print_prompt() {
+  std::cout << "$ ";
+}
+
+void read_input(std::string& input){
+  std::getline(std::cin, input);
+}
+
+void command(const std::string& input){
+  // Command dictionary does not exist yet
+  std::cout << input << " : command not found\n";
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  
   std::string input;
-  std::getline(std::cin, input);
 
-  std::cout << input << ": command not found\n";
+  while (true){
+    print_prompt();
+    read_input(input);
+    command(input);
+  }
+
 }
