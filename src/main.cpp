dc85aa559d7c856ab8aa8bdf1d313cd4c294286e
rakshitraj <hello@rakshitraj.com>
2024-06-30 04:10:57 +0530

    exit builtin

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <functional>
+
+// Predefined command return
+enum CommandResult {
+  COMMAND_SUCCESS,
+  COMMAND_FAILURE,
+  COMMAND_UNRECOGNIZED
+};
+
+// Functions that define command behaviour
+
+CommandResult cmd__exit(const std::string& input){
+  std::exit(EXIT_SUCCESS);
+}
+
+
+// Map of known commands and their implementaion
+std::map<std::string, std::function<CommandResult(const std::string&)>> command_map {
+  {"exit", cmd__exit}
+};
+
+// REPL
 
 void print_prompt() {
   std::cout << "$ ";
 }
 
+
 void read_input(std::string& input){
   std::getline(std::cin, input);
 }
 
-void command(const std::string& input){
-  // Command dictionary does not exist yet
-  std::cout << input << ": command not found\n";
+
+CommandResult do_command(const std::string& input){
+  std::map<std::string, std::function<CommandResult(const std::string&)>>::iterator it = command_map.find(input);
+  if (it != command_map.end()) {
+    it->second(input);
+  }
+  else {
+    return COMMAND_UNRECOGNIZED;
+  }
 }
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -23,7 +56,17 @@ int main() {
   while (true){
     print_prompt();
     read_input(input);
-    command(input);
+    
+    switch (do_command(input))
+    {
+      case COMMAND_SUCCESS:
+        continue;
+      case COMMAND_UNRECOGNIZED:
+        std::cout << input << ": command not recognized\n";
+        continue;
+      default:
+        std::cout << "unknown state\n";
+        std::exit(EXIT_FAILURE);
+    }
   }
-
 }
